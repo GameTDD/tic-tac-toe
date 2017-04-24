99e5ab06c19956ae3bf35f03b0e3e1351f6c7890
naomijub <jnboeira@outlook.com>
2017-04-24 00:31:00 -0300

    Basic Test Project

---
+++ GameContent/GameContent.Test/Test.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using GameContent;
+
+namespace GameContent.Test
+{
+	[TestFixture()]
+	public class Test
+	{
+		private Tester tester;
+
+		[TestFixtureSetUp()]
+		public void TestSetup()
+		{
+			tester = new Tester();
+		}
+
+		[Test()]
+		public void TesterShouldReturnTrueWhenIsBool()
+		{
+			Assert.That(true, Is.EqualTo(tester.isBool()));
+		}
+	}
+}
